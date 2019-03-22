26d2ba7425114a96a6db1b65574c9df3d61f0463
NikolaSlijpcevic <nikolaslijepcevic081296@gmail.com>
2019-03-22 22:43:37 +0100

    Manje ispravke

--- kt1/src/main/resources/static/scripts/pkiAdminScript.js
+++ kt1/src/main/resources/static/scripts/pkiAdminScript.js
@@ -120,7 +120,7 @@ $(document).ready(function($) {
      	}
      	
      	
-     	var b =$("#certificateCityInput option:selected").val();
+     	var b =$("#certificateCityInput").val();
      	var c =$("#certificateSoftwareModuleInput").val();
      	var d =$("#fromDatePicker").val();
       	var e =$("#toDatePicker").val();
