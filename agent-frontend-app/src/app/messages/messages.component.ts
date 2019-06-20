9d594152046efbb83fd8a96d076ab415ce57db8e
dejandoder <dejandoder@uns.ac.rs>
2019-06-21 01:04:21 +0200

    Komunikacija korisnik-agent

--- agent-frontend-app/src/app/messages/messages.component.ts
+++ agent-frontend-app/src/app/messages/messages.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../model/Message';
+import { MessageServiceService } from '../service/message-service.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-messages',
@@ -7,7 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessagesComponent implements OnInit {
 
-  constructor() { }
+  message : Message[] = [];
+
+  constructor(private messageService : MessageServiceService, private router : Router, private route: ActivatedRoute) { 
+    messageService.getAllMess().subscribe(
+      data => {
+        this.message = data;
+        messageService.setCurrentMessage(data);
+      }
+    )
+
+    setInterval(()=>{
+      this.message = messageService.getCurrentMessage();
+    }, 2000)
+  }
 
   ngOnInit() {
   }
---
+++ agent-frontend-app/src/app/model/Message.ts
@@ -0,0 +1,7 @@
+export class Message {
+    id : number;
+    content : string;
+    date : Date;
+    id_sender : number;
+    id_receiver : number;
+}
